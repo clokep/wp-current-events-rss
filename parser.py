0ea7c637f13b10b4a46bdc5e78ec7be2797ae802
Patrick Cloke <clokep@patrick.cloke.us>
2017-05-15 11:03:54 -0400

    Start a better composer.

--- parser.py
+++ parser.py
@@ -16,54 +16,66 @@ MARKUP_TO_HTML.update({
 })
 
 
-ARTICLE_URL_FORMAT = 'https://en.wikipedia.org/wiki/{}'
+class WikicodeToHtmlComposer(object):
+    """
+    Format HTML from Parsed Wikicode.
 
-def wiki_url(title):
-    """Given a page titiel, return a URL suitable for linking."""
-    return ARTICLE_URL_FORMAT.format(urllib.quote(title.encode('utf-8')))
+    Note that this is not currently re-usable.
+    """
+    def __init__(self, base_url='https://en.wikipedia.org/wiki'):
+        self._base_url = base_url
 
+        self._article_url_format = base_url + '/{}'
 
-def format_wikicode(obj):
-    """Returns Unicode of a Wikicode or Node object converted to HTML."""
-    if isinstance(obj, Wikicode):
-        result = u''
-        for node in obj.ifilter(recursive=False):
-            result += format_wikicode(node)
-        return result
+    def _get_url(self, title):
+        """Given a page title, return a URL suitable for linking."""
+        safe_title = urllib.quote(title.encode('utf-8'))
+        return self._article_url_format.format(safe_title)
 
-    if isinstance(obj, Tag):
-        # Convert all the children to HTML.
-        inner = u''.join(map(format_wikicode, obj.__children__()))
+    def compose(self, obj):
+        """Converts Wikicode or Node objects to HTML."""
+        if isinstance(obj, Wikicode):
+            result = u''
+            for node in obj.ifilter(recursive=False):
+                result += self.compose(node)
+            return result
 
-        # Self closing tags don't need an end tag, this produces "broken" HTML,
-        # but readers should handle it fine.
-        if not obj.self_closing:
-            template = u'<{tag}>{inner}</{closing_tag}>'
+        elif isinstance(obj, Tag):
+            # Convert all the children to HTML.
+            inner = u''.join(map(self.compose, obj.__children__()))
+
+            # Self closing tags don't need an end tag, this produces "broken"
+            # HTML, but readers should handle it fine.
+            if not obj.self_closing:
+                template = u'<{tag}>{inner}</{closing_tag}>'
+            else:
+                template = u'<{tag}>{inner}'
+
+            # Create an HTML tag.
+            # TODO Handle attributes.
+            return template.format(tag=obj.tag, inner=inner, closing_tag=obj.closing_tag)
+
+        elif isinstance(obj, Wikilink):
+            # Different text can be specified, or falls back to the title.
+            text = obj.text or obj.title
+            url = self._get_url(obj.title)
+            return u'<a href="{}">{}</a>'.format(url, self.compose(text))
+
+        elif isinstance(obj, ExternalLink):
+            # Different text can be specified, or falls back to the URL.
+            text = obj.title or obj.url
+            return u'<a href="{}">{}</a>'.format(obj.url, self.compose(text))
+
+        elif isinstance(obj, (list, tuple)):
+            # If the object is iterable, just handle each item separately.
+            result = u''
+            for node in obj:
+                result += self.compose(node)
+            return result
+
         else:
-            template = u'<{tag}>{inner}'
-
-        # Create an HTML tag.
-        # TODO Handle attributes.
-        return template.format(tag=obj.tag, inner=inner, closing_tag=obj.closing_tag)
-
-    elif isinstance(obj, Wikilink):
-        # Different text can be specified, or falls back to the title.
-        text = obj.text or obj.title
-        return u'<a href="{}">{}</a>'.format(wiki_url(obj.title), format_wikicode(text))
-
-    elif isinstance(obj, ExternalLink):
-        # Different text can be specified, or falls back to the URL.
-        text = obj.title or obj.url
-        return u'<a href="{}">{}</a>'.format(obj.url, format_wikicode(text))
-
-    elif isinstance(obj, (list, tuple)):
-        # If the object is iterable, just handle each item separately.
-        result = u''
-        for node in obj:
-            result += format_wikicode(node)
-        return result
-    else:
-        return unicode(obj)
+             # TODO Raise?
+            return unicode(obj)
 
 
 def filter_templates(node):
@@ -107,7 +119,7 @@ def get_articles():
 
     # Start at today.
     day = date.today()
-    
+
     for i in xrange(7):
         day -= timedelta(days=1)
 
@@ -117,9 +129,11 @@ def get_articles():
         wikicode = mwparserfromhell.parse(article)
         nodes = wikicode.filter(recursive=False, matches=filter_templates)
 
+        composer = WikicodeToHtmlComposer('https://en.wikipedia.org/wiki/')
+
         feed.add_item(title=u'Current events: {}'.format(day),
                       link=get_article_url(day),
-                      description=format_wikicode(nodes),
+                      description=composer.compose(nodes),
                       pubdate=datetime(*day.timetuple()[:3]))
 
     return feed.writeString('utf-8')
